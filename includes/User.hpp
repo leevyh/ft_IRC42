100a7a2d9ffaa5e05172d360e15009f11c83f0ad
Leevyh <leevyhaa@gmail.com>
2024-04-04 12:24:26 +0200

    class User - non testée

--- includes/User.hpp
+++ includes/User.hpp
@@ -1,7 +1,10 @@
 #pragma once
 #include <iostream>
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
 
-
+class Server;
 /* Each user is distinguished from other users by a unique nickname
 having a maximum length of nine (9) characters.  See the protocol
 grammar rules (section 2.3.1) for what may and may not be used in a
@@ -12,17 +15,30 @@ SHOULD accept longer strings as they may become used in future
 evolutions of the protocol. */
 class User {
 	private:
-		std::string _nick;
-		std::string _user;
-		std::string _passwrd;
+		User( void );
+
+		std::string _nickname;
+		std::string _username;
+		std::string _realname;
+		int _fd;
 
 	public:
-		User( void );
-		User( const User& rhs );
+		User( const int fd );
 		~User( void );
 
-		User&	operator=( const User& rhs );
+// SETTERS
+		void set_nickname( const std::string& nickname );
+		void set_username( const std::string& username );
+		void set_realname( const std::string& realname );
+
+// GETTERS
+		const std::string&	get_nickname( void ) const;
+		const std::string&	get_username( void ) const;
+		const std::string&	get_realname( void ) const;
+		int get_fd( void ) const;
 };
+
+std::ostream &operator<<( std::ostream &o, const User &src) ; 
 
 
 // nick ::=  <any characters except NUL, CR, LF, chantype character, and SPACE> <possibly empty sequence of any characters except NUL, CR, LF, and SPACE>
