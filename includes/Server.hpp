9d29da4618d5234fbcd6ec125dcfa9f456d485de
Neoblacks <antoine.mugnier035@gmail.com>
2024-04-05 17:52:34 +0200

    Fix Fd

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -15,29 +15,29 @@
 
 class Server
 {
-	public:
-		Server();
-		Server(long	port, std::string password);
-		Server(Server const & copy);
-		Server & operator=(Server const & rhs);
-		~Server();
+public:
+    Server();
+    Server(long	port, std::string password);
+    Server(Server const & copy);
+    Server & operator=(Server const & rhs);
+    ~Server();
 
-		void		init_serv(void);
-		long		get_Port(void) const;
-		std::string	get_Password(void) const;
-		std::vector <pollfd>	get_fds(void) const;
-		void		start_serv(void);
-		void	new_Connection_Client(void);
+    void		init_serv(void);
+    long		get_Port(void) const;
+    std::string	get_Password(void) const;
+    std::vector <pollfd>	get_fds(void) const;
+    void		start_serv(void);
+    void	new_Connection_Client(void);
 
-	private:
-		struct sockaddr_in	_server_addr;
-		long				_port;
-		int					_nb_of_users;
-		std::string			_password;
-		std::vector<pollfd>	_fds;
-		std::map<int, User > _fdusers;		  // users by fds
+private:
+    struct sockaddr_in	_server_addr;
+    long				_port;
+    int					_nb_of_users;
+    std::string			_password;
+    std::vector<pollfd>	pollfdmap;
+    std::map<int, User > clientmap;		  // users by fds
 
-		// std::map<std::string, User *> _users; // users by nickname
+    // std::map<std::string, User *> _users; // users by nickname
 
 
 };
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -4,30 +4,30 @@
 
 Server::Server()
 {
-	_port = 6667;
-	_password = "default";
-	_nb_of_users = 0;
+    _port = 6667;
+    _password = "default";
+    _nb_of_users = 0;
 }
 
 Server::Server(long port, std::string password) : _port(port), _password(password)
 {
-	std::cout << "Server created with port: " << _port << " and password: " << _password << std::endl;
-	_nb_of_users = 0;
+    std::cout << "Server created with port: " << _port << " and password: " << _password << std::endl;
+    _nb_of_users = 0;
 }
 
 Server::Server(Server const &copy)
 {
-	*this = copy;
+    *this = copy;
 }
 
 Server &Server::operator=(Server const &rhs)
 {
-	if (this != &rhs)
-	{
-		_port = rhs._port;
-		_password = rhs._password;
-	}
-	return (*this);
+    if (this != &rhs)
+    {
+        _port = rhs._port;
+        _password = rhs._password;
+    }
+    return (*this);
 }
 
 long Server::get_Port(void) const { return (_port); }
@@ -35,120 +35,121 @@ std::string Server::get_Password(void) const { return (_password); }
 
 void Server::init_serv(void)
 {
-	int server_socket;
-	int yes = 1;
-	char ip_addr[INET_ADDRSTRLEN];
+    int server_socket;
+    // int yes = 1;
+    // char ip_addr[INET_ADDRSTRLEN];
 
-	// char	msg[1024];
+    // char	msg[1024];
 
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-	if (server_socket == -1)
-	{
-		std::cerr << "Error creating socket" << std::endl;
-		// throw exception("Error creating socket")
-		exit(1);
-	}
-	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-	{
-		std::cerr << "Error Setsockopt" << std::endl;
-	}
+    if (server_socket == -1)
+    {
+        std::cerr << "Error creating socket" << std::endl;
+        // throw exception("Error creating socket")
+        exit(1);
+    }
+    // if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+    // {
+    // 	std::cerr << "Error Setsockopt" << std::endl;
+    // }
 
-	if (fcntl(server_socket, F_SETFL, O_NONBLOCK) == -1)
-	{
-		std::cerr << "Error setting socket to non-blocking" << std::endl;
-		// throw exception("Error setting socket to non-blocking")
-		exit(1);
-	}
+    if (fcntl(server_socket, F_SETFL, O_NONBLOCK) == -1)
+    {
+        std::cerr << "Error setting socket to non-blocking" << std::endl;
+        // throw exception("Error setting socket to non-blocking")
+        exit(1);
+    }
 
-	_server_addr.sin_family = AF_INET;
-	_server_addr.sin_port = htons(_port);
-	_server_addr.sin_addr.s_addr = INADDR_ANY;
+    _server_addr.sin_family = AF_INET;
+    _server_addr.sin_port = htons(_port);
+    _server_addr.sin_addr.s_addr = INADDR_ANY;
 
-	inet_ntop(AF_INET, &(_server_addr.sin_addr), ip_addr, INET_ADDRSTRLEN);
+    // inet_ntop(AF_INET, &(_server_addr.sin_addr), ip_addr, INET_ADDRSTRLEN);
 
-	if (bind(server_socket, (struct sockaddr *)&_server_addr, sizeof(_server_addr)) == -1)
-	{
-		std::cerr << "Error binding socket" << std::endl;
-		// throw exception("Error binding socket")
-		exit(1);
-	}
+    if (bind(server_socket, (struct sockaddr *)&_server_addr, sizeof(_server_addr)) == -1)
+    {
+        std::cerr << "Error binding socket" << std::endl;
+        // throw exception("Error binding socket")
+        exit(1);
+    }
 
-	if (listen(server_socket, 200) == -1)
-	{
-		std::cerr << "Error listening on socket" << std::endl;
-		// throw exception("Error listening on socket")
-		exit(1);
-	}
+    if (listen(server_socket, 200) == -1)
+    {
+        std::cerr << "Error listening on socket" << std::endl;
+        // throw exception("Error listening on socket")
+        exit(1);
+    }
 
-	std::cout << "End of init_serv" << std::endl;
-	std::cout << "Server listening on port " << _port << std::endl;
-	std::cout << "Socket: " << server_socket << std::endl;
-	std::cout << "Server address: " << inet_ntoa(_server_addr.sin_addr) << std::endl;
-	std::cout << "Server port: " << ntohs(_server_addr.sin_port) << std::endl;
-	_fds.push_back(pollfd());
-	_fds.back().fd = server_socket;
-	_fds.back().events = POLLIN;
+    // std::cout << "End of init_serv" << std::endl;
+    // std::cout << "Server listening on port " << _port << std::endl;
+    // std::cout << "Socket: " << server_socket << std::endl;
+    // std::cout << "Server address: " << inet_ntoa(_server_addr.sin_addr) << std::endl;
+    // std::cout << "Server port: " << ntohs(_server_addr.sin_port) << std::endl;
+    pollfdmap.push_back(pollfd());
+    pollfdmap.back().fd = server_socket;
+    pollfdmap.back().events = POLLIN;
 }
 
 void Server::start_serv(void)
 {
-	if (poll(&_fds[0], _fds.size(), 10000) == -1) // -1 means wait indefinitely but we can change it to a timeout 1000000 for 1 second
-	{
-		if (signal_value == false)
-		{
-			std::cerr << "Signal" << std::endl;
-			// throw exception("poll exception")
-			exit(1);
-		}
-	}
-	if (_fds[0].revents == POLLIN)
-		new_Connection_Client();
-	else
-	{
-		sleep(1);
-		// std::cout << "No new connection" << std::endl;
-	}
+    if (poll(&pollfdmap[0], pollfdmap.size(), 10000) == -1) // -1 means wait indefinitely but we can change it to a timeout 1000000 for 1 second
+    {
+        if (signal_value == false)
+        {
+            std::cerr << "Signal" << std::endl;
+            // throw exception("poll exception")
+            exit(1);
+        }
+    }
+    if (pollfdmap[0].revents == POLLIN)
+        new_Connection_Client();
+    else
+    {
+        sleep(1);
+        // std::cout << "No new connection" << std::endl;
+    }
 }
 
 void Server::new_Connection_Client(void)
 {
-	struct sockaddr_in user_addr;
-	socklen_t user_addr_len = sizeof(user_addr);
+    struct sockaddr_in user_addr;
+    socklen_t user_addr_len = sizeof(user_addr);
 
-	std::cout << "Socket server juste avant le fcntl :" << _fds[0].fd << std::endl;
+    std::cout << "Socket server juste avant le fcntl :" << pollfdmap[0].fd << std::endl;
 
-	int client_socket = accept(_fds[0].fd, (struct sockaddr *)&user_addr, &user_addr_len);
-	std::cout << "Client socket juste avant le fcntl :" << client_socket << std::endl;
+    int client_socket = accept(pollfdmap[0].fd, (struct sockaddr *)&user_addr, &user_addr_len);
+    std::cout << "Socket server juste apres le fcntl :" << pollfdmap[0].fd << std::endl;
+    std::cout << "Client socket juste avant le fcntl :" << client_socket << std::endl;
 
-	if (client_socket == -1)
-	{
-		std::cerr << "Error accepting connection" << std::endl;
-		// throw exception("Error accepting connection")
-		exit(1);
-	}
-	if (fcntl(client_socket, F_SETFL, O_NONBLOCK) == -1)
-	{
-		std::cerr << "Error setting connection to non-blocking" << std::endl;
-		// throw exception("Error setting connection to non-blocking")
-		exit(1);
-	}
-	std::cout << "New connection on client_socket: " << client_socket << std::endl;
-	std::cout << "User address: " << inet_ntoa(user_addr.sin_addr) << std::endl;
-	std::cout << "User port: " << ntohs(user_addr.sin_port) << std::endl;
-	std::cout << "User test: " << user_addr.sin_family << std::endl;
+    if (client_socket == -1)
+    {
+        std::cerr << "Error accepting connection" << std::endl;
+        // throw exception("Error accepting connection")
+        exit(1);
+    }
+    if (fcntl(client_socket, F_SETFL, O_NONBLOCK) == -1)
+    {
+        std::cerr << "Error setting connection to non-blocking" << std::endl;
+        // throw exception("Error setting connection to non-blocking")
+        exit(1);
+    }
+    // std::cout << "New connection on client_socket: " << client_socket << std::endl;
+    // std::cout << "User address: " << inet_ntoa(user_addr.sin_addr) << std::endl;
+    // std::cout << "User port: " << ntohs(user_addr.sin_port) << std::endl;
+    // std::cout << "User test: " << user_addr.sin_family << std::endl;
 
-	std::cout << "client socket :" << client_socket << std::endl;
+    std::cout << "client socket :" << client_socket << std::endl;
 
-	User user(client_socket);
-	std::cout << "Number of users: " << _nb_of_users << std::endl;
-	_fdusers[client_socket] = user;
-	_fds.push_back(pollfd());
-	_fds.back().fd = client_socket;
-	_fds.back().events = POLLIN;
-	_nb_of_users++;
-	std::cout << "Number of users: " << _nb_of_users << std::endl;
-	std::cout << "User : " << _fdusers[client_socket].get_fd() << std::endl;
+    // User user(client_socket);
+    // std::cout << "Number of users: " << _nb_of_users << std::endl;
+    // clientmap[client_socket] = user;
+    pollfdmap.push_back(pollfd());
+    pollfdmap.back().fd = client_socket;
+    pollfdmap.back().events = POLLIN;
+    // _nb_of_users++;
+    // std::cout << "Number of users: " << _nb_of_users << std::endl;
+    // std::cout << "User : " << clientmap[client_socket].get_fd() << std::endl;
 }
 
 Server::~Server()
